0f096fa59af6594482663415f7f5c2a719464fd3
Ed Simons <edsimons@me.com>
2018-10-15 14:24:51 +0100

    First set of predictions
    Round 9
    Wolverhampton 1 -  1 Watford
    West Ham 1 -  2 Tottenham
    Newcastle 1 -  1 Brighton
    Manchester City 2 -  1 Burnley
    Huddersfield 0 -  2 Liverpool
    Everton 1 -  1 Crystal Palace
    Chelsea 2 -  1 Manchester United
    Cardiff 2 -  2 Fulham
    Bournemouth 2 -  1 Southampton
    Arsenal 2 -  1 Leicester

--- predictor.py
+++ predictor.py
@@ -1,7 +1,32 @@
 import season
 import pprint
 
+
 season = season.Season()
 
-sr = season.league()
-pprint.pprint(sr)
+#sal = season.all_fixtures()
+#pprint.pprint(sal)
+
+#pprint.pprint(sr)
+
+#for round in range(1, season.number_of_rounds()+1):
+#    print(f'Round {round}')
+#    try:
+#        games = season.games_for_round(round)
+#    except ValueError as err:
+#        print(err)
+#    else:
+#        for g in games:
+#            print(f"{g['homeTeam']} {g['goalsHomeTeam']} - {g['goalsAwayTeam']} {g['awayTeam']}")
+
+next_round = season.first_unstarted_round()
+
+print(f'Round {next_round}')
+try:
+    games = season.games_for_round(next_round)
+except ValueError as err:
+    print(err)
+else:
+    for g in games:
+        print(season.predict_result(g))
+        
--- season.py
+++ season.py
@@ -19,12 +19,103 @@ import json
 
 # Transfer-Encoding: chunked
 
+def goals_average(goals_for, goals_against):
+    if not goals_for:
+        raise ValueError(f"Goals for is empty")
+
+    if not goals_against:
+        raise ValueError(f"Goals against is empty")
+
+    gf = (sum(goals_for)/len(goals_for))
+    ga = (sum(goals_against)/len(goals_against))
+
+    return round((gf + ga)/2)
+
 class Season:
     def __init__(self):
-        pass
+        self._all_games = list(json.load(open('league.json'))['api']['fixtures'].values())   # /fixtures/league/{league_id}
 
-    def league(self):
+    def all_games(self):
         """
-        /fixtures/league/{league_id}
+        Returns information for all games; played, live or unplayed.
         """
-        return json.load(open('league.json'))
+        return self._all_games
+
+    def number_of_rounds(self):
+        """
+        Gets the number of rounds that make up the season.
+        """
+        return len({fixture['round'] for fixture in self._all_games})
+
+    def games_for_round(self, round):
+        """
+        Returns information for all games in the round.
+        """
+        if round == 0 or round > self.number_of_rounds():
+            raise ValueError(f'"round" must be 1 to {self.number_of_rounds()} inclusive')
+
+        return [{'homeTeam': game['homeTeam'], 
+                 'awayTeam': game['awayTeam'],
+                 'goalsHomeTeam': game['goalsHomeTeam'],
+                 'goalsAwayTeam': game['goalsAwayTeam'],
+                 'status': game['status']} for game in self._all_games if game['round'] == f"Premier League - {round}"]
+
+    def all_games_unstarted(self, round):
+        """
+        Returns True if no games have been started for the round.
+        """
+        if round == 0 or round > self.number_of_rounds():
+            raise ValueError(f'"round" must be 1 to {self.number_of_rounds()} inclusive')
+
+        return all([game['status']=='Not Started' for game in self.games_for_round(round)])
+
+    def first_unstarted_round(self):
+        """
+        Returns the earliest round with no games started.
+
+        Note that during a typical round some games may have finished, some will be in progress and others not started.
+        """
+        for round in range(1, self.number_of_rounds()+1):
+            if self.all_games_unstarted(round):
+                return round 
+            
+    def predict_result(self, game):
+        """
+        Predicts the result of the game.
+        """
+        if game['status'] != 'Not Started':
+            raise ValueError(f"{game['homeTeam']} v {game['awayTeam']} has already started")
+
+        if game['goalsHomeTeam']:
+            raise ValueError(f"Home teams goals is invalid for unstarted match")
+        
+        if game['goalsAwayTeam']:
+            raise ValueError(f"Away teams goals is invalid for unstarted match")
+
+
+        homeTeamGoalsFor = list()
+        homeTeamGoalsAgainst = list()
+        awayTeamGoalsFor = list()
+        awayTeamGoalsAgainst = list()
+
+        for round in range(1, self.first_unstarted_round()):
+            for games in self.games_for_round(round):
+                if games['homeTeam'] == game['homeTeam']:
+                    homeTeamGoalsFor.append(int(games['goalsHomeTeam']))
+                    homeTeamGoalsAgainst.append(int(games['goalsAwayTeam']))
+                    
+                if games['awayTeam'] == game['awayTeam']:
+                    awayTeamGoalsFor.append(int(games['goalsAwayTeam']))
+                    awayTeamGoalsAgainst.append(int(games['goalsHomeTeam']))
+
+        homeTeamGoals = goals_average(homeTeamGoalsFor, awayTeamGoalsAgainst)
+        awayTeamGoals = goals_average(homeTeamGoalsAgainst, awayTeamGoalsFor)
+        
+        return f"{game['homeTeam']} {homeTeamGoals} -  {awayTeamGoals} {game['awayTeam']}"
+        
+
+
+
+
+        
+
